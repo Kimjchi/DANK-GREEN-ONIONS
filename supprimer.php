93d463f024b524b9c85de8683adfbd809b2b82a9
arctangente <ngutam91@gmail.com>
2017-06-09 00:00:53 +0200

    Commit j'ai oublié un fichier dans le commit précédent lol

--- supprimer.php
+++ supprimer.php
@@ -1,5 +1,7 @@
 <?php
+$BDD = new PDO('mysql:host=localhost;dbname=projet lo07', 'root','', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 $requete = $BDD->prepare('DELETE FROM `appartient` WHERE idFormation=? AND sigle=?');
+$requete->execute(array($_POST['idFormation'],$_POST['supprimer']));
 
 
 
