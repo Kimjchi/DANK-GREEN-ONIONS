a0b0c822d04036b9318694ace088d2334941693e
Kimjchi <kimj77600@gmail.com>
2017-06-08 16:33:31 +0200

    Fix bug pour l'import
    L'import marche et on ajoute un fichier afficher cursus pour aérer la
    page modification_cursus

---
+++ affichage_cursus.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Affichage du cursus</title>
+	<meta charset="utf-8">
+</head>
+<body>
+  <?php
+  $idFormation2=array();
+  $sem_label=array();
+
+  $reponse9 = $BDD->prepare('SELECT `idFormation`, `sem_label` FROM `formation` WHERE `idCursus`= ? ORDER BY `sem_label`');
+  $reponse9->execute(array($_POST['idCursus']));
+        while ($formation10 = $reponse9->fetch())
+        {
+          $idFormation2[]=$formation10['idFormation'];
+          $sem_label[]=$formation10['sem_label'];
+        }
+        $reponse9->closeCursor();
+
+  ?>
+
+
+    <table class="table table-striped">
+      <thead>
+        <td></td><td> CS </td><td> TM </td><td> ST </td><td> EC </td><td >ME </td><td> CT </td><td> HP </td><td> NPML </td>
+      </thead>
+      <tbody>
+      <?php
+        $i = 0;
+        foreach ($idFormation2 as $element) {
+
+          $uv_cs=array();
+          $uv_tm=array();
+          $uv_st=array();
+          $uv_ec=array();
+          $uv_me=array();
+          $uv_ct=array();
+          $uv_hp=array();
+          $npml_admis = FALSE;
+
+          $reponse3 = $BDD->prepare('SELECT * FROM `appartient` a,`element_de_formation` e WHERE `idFormation`= ? AND a.`sigle` = e.`sigle` ORDER BY `categorie`');
+          $reponse3->execute(array($element));
+                while ($appartient2 = $reponse3->fetch())
+                {
+                  switch($appartient2['categorie']){
+                    case "CS":
+                      $uv_cs[$appartient2['sigle']]=$appartient2['resultat'];
+                      break;
+                    case "TM":
+                      $uv_tm[$appartient2['sigle']]=$appartient2['resultat'];
+                      break;
+                    case "ST":
+                      $uv_st[$appartient2['sigle']]=$appartient2['resultat'];
+                      break;
+                    case "EC":
+                      $uv_ec[$appartient2['sigle']]=$appartient2['resultat'];
+                      break;
+                    case "ME":
+                      $uv_me[$appartient2['sigle']]=$appartient2['resultat'];
+                      break;
+                    case "CT":
+                      $uv_ct[$appartient2['sigle']]=$appartient2['resultat'];
+                      break;
+                    case "HP":
+                      $uv_hp[$appartient2['sigle']]=$appartient2['resultat'];
+                      break;
+                    case "NPML":
+                      if ($appartient2['resultat']=="ADM") {
+                        $npml_admis=TRUE;
+                      }
+                      break;
+                  }
+                }
+                $reponse3->closeCursor();
+
+          echo "<tr>";
+          echo "<td>".$sem_label[$i]."</td>";
+          echo "<td>";
+            foreach ($uv_cs as $key => $value) {
+              echo $key." ".$value."<br>";
+            }
+          echo"</td>";
+          echo "<td>";
+            foreach ($uv_tm as $key => $value) {
+              echo $key." ".$value."<br>";
+            }
+          echo"</td>";
+          echo "<td>";
+            foreach ($uv_st as $key => $value) {
+              echo $key." ".$value."<br>";
+            }
+          echo"</td>";
+          echo "<td>";
+            foreach ($uv_ec as $key => $value) {
+              echo $key." ".$value."<br>";
+            }
+          echo"</td>";
+          echo "<td>";
+            foreach ($uv_me as $key => $value) {
+              echo $key." ".$value."<br>";
+            }
+          echo"</td>";
+          echo "<td>";
+            foreach ($uv_ct as $key => $value) {
+              echo $key." ".$value."<br>";
+            }
+          echo"</td>";
+          echo "<td>";
+            foreach ($uv_hp as $key => $value) {
+              echo $key." ".$value."<br>";
+            }
+          echo"</td>";
+          echo "<td>";
+            if($npml_admis){
+              echo "BULE ADM";
+            }
+          echo"</td>";
+
+
+          echo "</tr>";
+
+          echo "<tr>";
+
+          echo "<td>";
+          echo "Total semestre";
+          echo "</td>";
+
+          echo "<td>";
+          echo compteCredits('CS',$element);
+          echo "</td>";
+
+          echo "<td>";
+          echo compteCredits('TM',$element);
+          echo "</td>";
+
+          echo "<td>";
+          echo compteCredits('ST',$element);
+          echo "</td>";
+
+          echo "<td>";
+          echo compteCredits('EC',$element);
+          echo "</td>";
+
+          echo "<td>";
+          echo compteCredits('ME',$element);
+          echo "</td>";
+
+          echo "<td>";
+          echo compteCredits('CT',$element);
+          echo "</td>";
+
+          echo "<td>";
+          echo compteCredits('HP',$element);
+          echo "</td>";
+
+          echo "</tr>";
+
+          $i++;
+        }
+
+      ?>
+      </tbody>
+    </table>
+</body>
+</html>
--- import.php
+++ import.php
@@ -5,59 +5,97 @@ $csv = file_get_contents($filename);
 $csv_lines = preg_split('/\\r\\n|\\r|\\n/', $csv);
 print_r($csv_lines);
 
+
+
+
+
+
+
+
 // on explode les elements du csv en variables
 
 foreach ($csv_lines as $element) {
-list($sem_seq, $sem_label , $sigle, $categorie, $credit, $affectation, $utt, $profil, $resultat)= explode(",", $element);
-echo $sem_seq;
+    list($sem_seq, $sem_label , $sigle, $categorie, $credit, $affectation, $utt, $profil, $resultat)= explode(",", $element);
+    echo $sigle;
+
+    //On regarde si on a le même label de semestre dans le cursus choisi
+    $sem = array();
+    $reponse4 = $BDD->prepare('SELECT `sem_label` FROM `formation` WHERE `idCursus` = ?');
+    $reponse4->execute(array($_POST['idCursus']));
+    while ($sem_liste = $reponse4->fetch())
+    {
+        $sem[]=$sem_liste['sem_label'];
+    }
+
+    $sigle_bdd = array(); //on crée un array pour mettre les datas dedans
+    $reponse = $BDD->query('SELECT sigle FROM element_de_formation');
+    while ($element_de_formation_bdd=$reponse->fetch()) {
+        $sigle_bdd[]=$element_de_formation_bdd['sigle'];
+    }
+
+    $identique=FALSE;
+    foreach ($sem as $element) {
+    //Si le sem existe déjà dans le cursus, on lie l'uv choisi à la table qui existe déjà 
+        if ($element == $sem_label) {
+          $identique = TRUE;
+        }
+    }
+    if ($identique) {
+        $reponse5 = $BDD->prepare('SELECT `idFormation` FROM `formation` WHERE `idCursus` = ? AND `sem_label` = ?;');
+        $reponse5->execute(array($_POST['idCursus'],$sem_label));
+        while ($idFormation_existe = $reponse5->fetch())
+        {
+           $idFormation=$idFormation_existe['idFormation'];
+        }
+        $reponse5->closeCursor();
+    }
+    //Sinon on crée une nouvelle formation
+    else {
+
+        $requete2 = $BDD->prepare('INSERT INTO `formation`(`idCursus`, `sem_label`) VALUES (?,?)');
+        $requete2->execute(array($_POST['idCursus'], $sem_label)); //idcursus est la car il est hidden
+
+        $reponse2 = $BDD->query('SELECT MAX(`idFormation`) FROM `formation`');
+            while ($formation = $reponse2->fetch()){
+                $idFormation=$formation['MAX(`idFormation`)'];
+              }
+        $reponse2->closeCursor();
+        $requete2->closeCursor();
+    }
+    $present = false ;
+
+
+    foreach ($sigle_bdd as $element2) {
+    if ($sigle==$element2) {
+        $present = true;
+        }
+    }
+      if ($present){ //si le sigle existe  déja dans la bdd
+
+        //On met dans la table appartient
+
+            $requete3 = $BDD->prepare('INSERT INTO `appartient`(`sem_seq`, `sigle`, `affectation`, `utt`, `profil`, `resultat`, `idFormation`)  VALUES (?,?,?,?,?,?,?)');
+            $requete3->execute(array($sem_seq, strval($sigle) , strval($affectation), strval($utt), strval($profil), strval($resultat), $idFormation));
+
+            $requete3->closeCursor();
+        }
+
+    else { //on ajoute l'uv dans la bdd
+
+        $requete3 = $BDD->prepare('INSERT INTO `element_de_formation`(`sigle`, `categorie`, `credit`) VALUES (?,?,?)');
+        $requete3->execute(array(strval($sigle), strval($categorie), $credit));
+
+        $requete3 = $BDD->prepare('INSERT INTO `appartient`(`sem_seq`, `sigle`, `affectation`, `utt`, `profil`, `resultat`, `idFormation`)  VALUES (?,?,?,?,?,?,?)');
+        $requete3->execute(array($sem_seq, strval($sigle) , strval($affectation), strval($utt), strval($profil), strval($resultat), $idFormation));
+        $requete3->closeCursor();
+
+        }
 }
 
 
-$sigle_bdd = array(); //on crée un array pour mettre les datas dedans
-$reponse = $BDD->query('SELECT sigle FROM element_de_formation');
-while ($element_de_formation_bdd=$reponse->fetch()) {
-	$sigle_bdd[]=$element_de_formation_bdd['sigle'];
-}
-
-// on crée une nouvelle formation
-
-$requete2 = $BDD->prepare('INSERT INTO `formation`(`idCursus`, `sem_label`) VALUES (?,?)');
-$requete2->execute(array($_POST['idCursus'], $sem_label)); //idcursus est la car il est hidden
-
-$reponse2 = $BDD->query('SELECT MAX(`idFormation`) FROM `formation`');
-    while ($formation = $reponse2->fetch()){
-        $idFormation=$formation['MAX(`idFormation`)'];
-      }
-$reponse2->closeCursor();
-$requete2->closeCursor();
-$present = false ;
 
 
-foreach ($sigle_bdd as $element2) {
-if ($sigle==$element2) {
-	$present = true;
-	}
-}
-  if ($present){ //si le sigle existe  déja dans la bdd
 
-    //On met dans la table appartient
-
-    	$requete3 = $BDD->prepare('INSERT INTO `appartient`(`sem_seq`, `sigle`, `affectation`, `utt`, `profil`, `resultat`, `idFormation`)  VALUES (?,?,?,?,?,?,?)');
-    	$requete3->execute(array($sem_seq, $sigle , $affectation, $utt, $profil, $resultat, $idFormation));
-
-    	$requete3->closeCursor();
-	}
-
-else { //on ajoute l'uv dans la bdd
-
-    $requete3 = $BDD->prepare('INSERT INTO `element_de_formation`(`sigle`, `categorie`, `credit`) VALUES (?,?,?)');
-    $requete3->execute(array($sigle, $categorie, $credit));
-
-    $requete3 = $BDD->prepare('INSERT INTO `appartient`(`sem_seq`, `sigle`, `affectation`, `utt`, `profil`, `resultat`, `idFormation`)  VALUES (?,?,?,?,?,?,?)');
-    $requete3->execute(array($sem_seq, $sigle , $affectation, $utt, $profil, $resultat, $idFormation));
-    $requete3->closeCursor();
-
-	}
 
 
 
